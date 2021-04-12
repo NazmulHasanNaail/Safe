f817db8fd6626db41b81c33ba1a12bb8b6a2f8ab
Nazmul <nazmulhasanfci@gmail.com>
2021-04-13 02:47:04 +0600

    add info sidebar and banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,65 @@
 (function($) {
 	
 	"use strict";
-	
+
+	//Mobile Nav Hide Show
+	if($('.mobile-menu').length){
+		
+		$('.mobile-menu .menu-box').mCustomScrollbar();
+		
+		var mobileMenuContent = $('.main-header .menu-area .main-menu').html();
+		$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
+		$('.sticky-header .main-menu').append(mobileMenuContent);
+		
+		//Dropdown Button
+		$('.mobile-menu li.dropdown .dropdown-btn').on('click', function() {
+			$(this).toggleClass('open');
+			$(this).prev('ul').slideToggle(500);
+		});
+		//Dropdown Button
+		$('.mobile-menu li.dropdown .dropdown-btn').on('click', function() {
+			$(this).prev('.megamenu').slideToggle(900);
+		});
+		//Menu Toggle Btn
+		$('.mobile-nav-toggler').on('click', function() {
+			$('body').addClass('mobile-menu-visible');
+		});
+
+		//Menu Toggle Btn
+		$('.mobile-menu .menu-backdrop,.mobile-menu .close-btn').on('click', function() {
+			$('body').removeClass('mobile-menu-visible');
+		});
+	}
+
+	// banner-carousel
+	if ($('.banner-carousel').length) {
+        $('.banner-carousel').owlCarousel({
+            loop:true,
+			margin:0,
+			// nav:false,
+			dots:false,
+			animateOut: 'fadeOut',
+    		animateIn: 'fadeIn',
+    		active: true,
+			smartSpeed: 1000,
+			autoplay: 6000,
+            // navText: [ '<span class="icon-Left-arrow"></span>', '<span class="icon-Right-arrow"></span>' ],
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                800:{
+                    items:1
+                },
+                1024:{
+                    items:1
+                }
+            }
+        });
+    }
 	
 
 })(window.jQuery);
