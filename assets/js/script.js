fb74e85126b99d8ffa3bcdada74b3fca533784c1
Nazmul <nazmulhasanfci@gmail.com>
2021-04-13 23:21:08 +0600

    add about section

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,6 +60,51 @@
             }
         });
     }
+	//paroller
+	if($('.paroller').length){
+		$('.paroller').paroller({
+			  factor: 0.1,            // multiplier for scrolling speed and offset, +- values for direction control  
+			  factorLg: 0.1,          // multiplier for scrolling speed and offset if window width is less than 1200px, +- values for direction control  
+			  type: 'foreground',     // background, foreground  
+			  direction: 'vertical' // vertical, horizontal  
+		});
+	}
+
+	if($('.paroller-2').length){
+		$('.paroller-2').paroller({
+			  factor: -0.1,            // multiplier for scrolling speed and offset, +- values for direction control  
+			  factorLg: -0.1,          // multiplier for scrolling speed and offset if window width is less than 1200px, +- values for direction control  
+			  type: 'foreground',     // background, foreground  
+			  direction: 'vertical' // vertical, horizontal  
+		});
+	}
+	//Fact Counter + Text Count
+	if($('.count-box').length){
+		$('.count-box').appear(function(){
 	
+			var $t = $(this),
+				n = $t.find(".count-text").attr("data-stop"),
+				r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+				
+			if (!$t.hasClass("counted")) {
+				$t.addClass("counted");
+				$({
+					countNum: $t.find(".count-text").text()
+				}).animate({
+					countNum: n
+				}, {
+					duration: r,
+					easing: "linear",
+					step: function() {
+						$t.find(".count-text").text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$t.find(".count-text").text(this.countNum);
+					}
+				});
+			}
+			
+		},{accY: 0});
+	}
 
 })(window.jQuery);
